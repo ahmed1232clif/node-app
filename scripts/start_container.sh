32c314f965aa31f86ad42926ca8f70eeb14d53cb
ahmed1232clif <79502923+ahmed1232clif@users.noreply.github.com>
2023-11-30 15:20:33 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 881015300215.dkr.ecr.us-east-1.amazonaws.com
+cd node-app
 
-docker pull 881015300215.dkr.ecr.us-east-1.amazonaws.com/node:latest
-
-docker run -d -p 80:80 881015300215.dkr.ecr.us-east-1.amazonaws.com/node:latest
+docker-compose up
